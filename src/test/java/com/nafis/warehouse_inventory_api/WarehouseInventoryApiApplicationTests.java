8af69c25415231c375bc5e58a750d13a7bbda069
Nafis Tahmid <nafistahmidn@gmail.com>
2026-03-03 03:54:56 +0000

    Iniitialize Spring Boot project

---
+++ src/test/java/com/nafis/warehouse_inventory_api/WarehouseInventoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nafis.warehouse_inventory_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseInventoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
